c21041e6949288e6fc35f56fb04250c1fa8d1d55
Andrew P <386155@colum.edu>
2023-09-20 13:25:32 -0500

    comments, small refactoring

--- Assets/script/stateMachine/ModeBehavior.cs
+++ Assets/script/stateMachine/ModeBehavior.cs
@@ -22,14 +22,14 @@ public class ModeBehavior : MonoBehaviour
 
     /// <summary>
     /// TODO:
-    /// - Add timer for Ball Saver (COROUTINE!!!!)
+    /// - Add timer for Ball Saver (COROUTINE!!!!) - done
     /// - Make sure ConsumeBall does not make the ball count go down when saver is active
     /// - Make the holes and jumper bumps increase score.
     /// - When one of the hole entry count reaches 3, reset the counter for all 3 holes and then start the mode.
     ///   (and when a mode that is NOT normal is happening, do NOT make the counter go up)
     /// - When any other mode is active that is NOT Normal/Multiball/Crack, make the ball saver timer infinite,
     ///   but also start the mode timer, and when that reaches 0 go to END_MODE, calculate score, increase multiplier
-    ///   state, then NORMAL.
+    ///   state, then NORMAL. -- Most of this is done, what's left is score calculation.
     /// - When ball is consumed and saver is off, multiply the score with the Multiplier. Respawn ball, re-enable ball saver and it's timer.
     /// </summary>
 
@@ -116,6 +116,7 @@ public class ModeBehavior : MonoBehaviour
     private void revertModeToNormal()
     {
         modeState = currentMode.NORMAL;
+        secondsUntilBallSaveEnds = predefined_secondsUntilModeEnds;
         timerCountdownStart();
     }
 }

d773e9df6d0012c19f4a034951cd213f412db39a
Andrew P <386155@colum.edu>
2023-10-15 17:39:15 -0500

    Made test OverRamp

--- Assets/script/TableScripts/OverRampLayerChanger.cs
+++ Assets/script/TableScripts/OverRampLayerChanger.cs
@@ -6,12 +6,18 @@ public class OverRampLayerChanger : MonoBehaviour
 {
     public GameObject Pinball;
     private Rigidbody2D ballRigidbody;
+    [Tooltip("Marks the OverRamp's entrance so the ball can change its Collision Layer")]
     public bool isEntrance;
+    [Tooltip("Marks the OverRamp's entrance so the ball can change back to its original Collision Layer")]
     public bool isExit;
+    [Tooltip("Set the ball to a certain Layer defined by the designer.")]
     public int LayerToSetPinball;
+    [Tooltip("Set the ball back to its previous Layer defined by the designer. Recommended value: 0")]
     public int OriginalLayerToSetPinball;
+    [Tooltip("The direction you want the ball to go to. You can select any GameObject and it will use the Rotation of that object to set the ball's respectively.")]
     public Transform BallDirection;
-    private float magnitude_forRB;
+    [Tooltip("How fast you want the ball Velocity to be. Recommended value: 20. If you don't want to change it's speed, set it to 1.")]
+    public int velocityMultiplier;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +38,7 @@ public class OverRampLayerChanger : MonoBehaviour
                 Pinball.transform.rotation = BallDirection.rotation;
                 Pinball.layer = LayerToSetPinball;
                 Debug.Log($"Pinball layer is now {Pinball.layer}.");
-                ballRigidbody.AddForce(Vector2.one * magnitude_forRB, ForceMode2D.Impulse);
+                ballRigidbody.velocity = Vector2.one * velocityMultiplier;
 
             }
             if (isExit)

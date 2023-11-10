c39226984e0f0332f71cd9de3d7c3bb52f95f206
Andrew P <386155@colum.edu>
2023-11-09 19:26:45 -0600

    implemented singleton (2)

---
+++ Assets/script/sgt/globalScoreBehavior.cs
@@ -0,0 +1,9 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class globalScoreBehavior : Singleton<globalScoreBehavior>
+{
+    public int global_pl_score;
+}

98a268dbbe1850cde721409a6e160a1a7a5ac57a
Andrew P <386155@colum.edu>
2023-11-16 15:58:15 -0600

    Update RhythmManager.cs

--- Assets/script/Rhythm/RhythmManager.cs
+++ Assets/script/Rhythm/RhythmManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Rhythm Program made by Mack Pearson-Muggli.
+/// RHYTHM PROGRAM made by Mack Pearson-Muggli.
 /// Used with permission.
 /// </summary>
 

e2ddf279d9364c3d5873558ed9c7a0547ea454f7
Andrew P <386155@colum.edu>
2023-11-30 12:25:23 -0600

    sprite change to bumpers on collision

--- Assets/script/TableScripts/JumperBehavior.cs
+++ Assets/script/TableScripts/JumperBehavior.cs
@@ -7,6 +7,8 @@ public class JumperBehavior : MonoBehaviour
 {
     public tableTally tally;
     public Text statusText;
+    [SerializeField] private GameObject NonHitChildSprite;
+    [SerializeField] private GameObject OnHitChildSprite;
 
     private void Start()
     {
@@ -16,7 +18,21 @@ public class JumperBehavior : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         tally.tallyBumper();
+        StartCoroutine(spriteChange());
         StartCoroutine(changeStatusMessage());
+    }
+
+    IEnumerator spriteChange() //change the sprites which are GameObjects. Simple enable/disable.
+    {
+        if(NonHitChildSprite != null && OnHitChildSprite != null) //if they are null leave it alone and dont get the console angry.
+        {
+            NonHitChildSprite.SetActive(false);
+            OnHitChildSprite.SetActive(true);
+            yield return new WaitForSecondsRealtime(0.05f);
+            NonHitChildSprite.SetActive(true);
+            OnHitChildSprite.SetActive(false);
+        }
+        yield return null;
     }
 
     IEnumerator changeStatusMessage()

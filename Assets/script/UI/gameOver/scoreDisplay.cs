43a0680a6863e80cdfa0de9f4f4e8781fcfc61be
Andrew P <386155@colum.edu>
2023-12-01 14:31:06 -0600

    start to retry on game over screen

--- Assets/script/UI/gameOver/scoreDisplay.cs
+++ Assets/script/UI/gameOver/scoreDisplay.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class scoreDisplay : MonoBehaviour
 {
     public Text score;
+    private PlayerInput pl_input;
     // Start is called before the first frame update
     void Start()
     {
+        pl_input = GetComponent<PlayerInput>();
         score.text = $"{globalScoreBehavior.Instance.global_pl_score}";
+    }
+
+    void OnPauseButton()
+    {
+        SceneManager.LoadScene("TestTable");
     }
 
     // Update is called once per frame

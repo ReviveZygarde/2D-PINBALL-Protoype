d81b8ecf05f43a2d8f94d40dfbde6d35eb3b52d3
Andrew P <386155@colum.edu>
2023-12-12 21:57:53 -0600

    push commit before i start crashing the thing again

--- Assets/script/TableScripts/saverIndicatorBehavior.cs
+++ Assets/script/TableScripts/saverIndicatorBehavior.cs
@@ -5,9 +5,35 @@ using UnityEngine;
 public class saverIndicatorBehavior : MonoBehaviour
 {
     public GameObject SAVER_OK;
-    public GameObject SAVER_ALMOST_END;
     public GameObject SAVER_OFF;
     public ModeBehavior common_ModeBehavior;
+
+    private void Start()
+    {
+        StartCoroutine(indicatorFlash());
+    }
+
+    IEnumerator indicatorFlash() //just to make the corotutine constantly run every second if it's not in ALMOST_END mode. also to prevent the coroutine from
+                                 //actually stopping.
+    {
+        while(common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.ON)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        while (common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.OFF)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        while (common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.ALMOST_END)
+        {
+            SAVER_OK.SetActive(true);
+            yield return new WaitForSecondsRealtime(0.25f);
+            SAVER_OK.SetActive(false);
+            SAVER_OFF.SetActive(true);
+            yield return new WaitForSecondsRealtime(0.25f);
+            SAVER_OFF.SetActive(false);
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,19 +41,11 @@ public class saverIndicatorBehavior : MonoBehaviour
         if(common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.ON)
         {
             SAVER_OK.SetActive(true);
-            SAVER_ALMOST_END.SetActive(false);
-            SAVER_OFF.SetActive(false);
-        }
-        if (common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.ALMOST_END)
-        {
-            SAVER_OK.SetActive(false);
-            SAVER_ALMOST_END.SetActive(true);
             SAVER_OFF.SetActive(false);
         }
         if (common_ModeBehavior.ballSaverState == ModeBehavior.ballSaver.OFF)
         {
             SAVER_OK.SetActive(false);
-            SAVER_ALMOST_END.SetActive(false);
             SAVER_OFF.SetActive(true);
         }
     }

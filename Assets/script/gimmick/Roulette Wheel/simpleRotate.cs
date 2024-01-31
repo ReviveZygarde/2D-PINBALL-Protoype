513f639b8253e5cf4e546ac31fce9a14411faa52
Andrew P <386155@colum.edu>
2024-01-30 19:12:55 -0600

    Update simpleRotate.cs

--- Assets/script/gimmick/Roulette Wheel/simpleRotate.cs
+++ Assets/script/gimmick/Roulette Wheel/simpleRotate.cs
@@ -6,11 +6,6 @@ public class simpleRotate : MonoBehaviour
 {
     [SerializeField] private float speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()

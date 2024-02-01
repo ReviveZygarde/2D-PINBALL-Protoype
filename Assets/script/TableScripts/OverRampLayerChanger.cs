f0455608f6abc00792ea81aca787ef646992e401
Andrew P <386155@colum.edu>
2024-02-01 15:33:54 -0600

    a

--- Assets/script/TableScripts/OverRampLayerChanger.cs
+++ Assets/script/TableScripts/OverRampLayerChanger.cs
@@ -37,6 +37,7 @@ public class OverRampLayerChanger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Pinball = GameObject.Find("Pinball");
         ballRigidbody = Pinball.GetComponent<Rigidbody2D>();
     }
 
---
+++ Assets/script/TableScripts/dashPanel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dashPanel : OverRampLayerChanger //wow! actually using Inheritance!
+{
+    private Rigidbody2D ballRigidbody;
+
+    /*
+     * Inherited generic dash panel for the pinball when
+     * it lands on it
+     */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pinball = GameObject.Find("Pinball");
+        ballRigidbody = Pinball.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == Pinball)
+        {
+            Pinball.transform.position = this.transform.position;
+            ballRigidbody.velocity = ballDirection * velocityMultiplier;
+        }
+    }
+}

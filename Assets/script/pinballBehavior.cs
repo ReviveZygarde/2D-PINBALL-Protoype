3538c048cf100493f128cb2e34376c867dd23fc4
Andrew P <386155@colum.edu>
2024-02-20 18:07:13 -0600

    now ball setting has a function

--- Assets/script/pinballBehavior.cs
+++ Assets/script/pinballBehavior.cs
@@ -6,11 +6,16 @@ public class pinballBehavior : MonoBehaviour
 {
     //Empty script...
     //TODO: get information from a setting/game option singleton that tells the rigidbody mass?
+    private Rigidbody2D ballRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        if(globalSetting.Instance.ballSetting == globalSetting.ballMass.LIGHT)
+        {
+            ballRigidbody.mass = ballRigidbody.mass - 1f;
+        }
     }
 
     // Update is called once per frame

6aa5fe3eac18050538a73cd8f4f5e0c8ccc10ade
Andrew P <386155@colum.edu>
2024-03-01 00:39:36 -0600

    something drastic gonna happen again probably

---
+++ Assets/script/gimmick/railway/railwayManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class railwayManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] lines;
+    [SerializeField] private int initializedTimeLeft = 15; //15 is the default value
+    [SerializeField] private int timeLeft;
+    [SerializeField] private int currentIndexOfArray;
+    [SerializeField] private GameObject pinball;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pinball = GameObject.Find("Pinball");
+        timeLeft = initializedTimeLeft;
+        changeLines();
+    }
+
+    IEnumerator waitToChangeLines()
+    {
+        while(timeLeft > 0)
+        {
+            timeLeft--;
+            yield return new WaitForSeconds(1f);
+        }
+
+        if (timeLeft <= 0)
+        {
+            timeLeft = 0;
+            while(pinball.layer != 0)
+            {
+                if (pinball.layer == 0)
+                {
+                    changeLines();
+                    timeLeft = initializedTimeLeft;
+                }
+                yield return new WaitForSeconds(0.25f);
+            }
+        }
+    }
+
+    void changeLines()
+    {
+        foreach(GameObject line in lines)
+        {
+            line.SetActive(false);
+        }
+        lines[currentIndexOfArray].SetActive(true);
+        currentIndexOfArray++;
+        if(currentIndexOfArray >= lines.Length)
+        {
+            currentIndexOfArray = 0;
+        }
+        StopAllCoroutines();
+        StartCoroutine(waitToChangeLines());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

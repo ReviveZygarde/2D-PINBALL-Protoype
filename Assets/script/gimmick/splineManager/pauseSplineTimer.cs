d920a39c561a222ea15c758b42bc8701fb47a9cb
Andrew P <386155@colum.edu>
2024-04-05 00:24:13 -0500

    TODO: Rush Mode Ramps

--- Assets/script/gimmick/splineManager/pauseSplineTimer.cs
+++ Assets/script/gimmick/splineManager/pauseSplineTimer.cs
@@ -12,11 +12,13 @@ public class pauseSplineTimer : MonoBehaviour
     public float waitTimer;
     public Text uiOverheadText;
     [SerializeField] GameObject laserBeam;
+    private ModeBehavior mode;
 
     // Start is called before the first frame update
     void Start()
     {
         pinball = GameObject.Find("Pinball");
+        mode = GameObject.Find("common").GetComponent<ModeBehavior>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,7 +35,15 @@ public class pauseSplineTimer : MonoBehaviour
         {
             splineAnimator.Pause();
         }
-        uiOverheadText.text = $"BUMPERS PAUSED FOR {waitTimer} SEC. GO! GO! GO!";
+        if(mode.modeState == ModeBehavior.currentMode.RUSH)
+        {
+            mode.secondsUntilModeEnds = mode.secondsUntilModeEnds + 10;
+            uiOverheadText.text = $"TIME EXTENSION! +10 SEC.";
+        }
+        if(mode.modeState != ModeBehavior.currentMode.RUSH)
+        {
+            uiOverheadText.text = $"BUMPERS PAUSED FOR {waitTimer} SEC. GO! GO! GO!";
+        }
         laserBeam.SetActive(false);
         yield return new WaitForSecondsRealtime(waitTimer);
         foreach (SplineAnimate splineAnimator in splineAnimators)
